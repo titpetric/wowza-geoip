638865f90d5312f0e31889556554efd40f82bf7b
William Hetherington <will@netromedia.com>
2011-09-02 12:40:01 -0700

    Added support for GeoLiteCity DB, in place of Country DB. Added types "city" & "region"

--- src/GeoIPLookupService.java
+++ src/GeoIPLookupService.java
@@ -3,9 +3,11 @@
 Tit Petric, Monotek d.o.o., (cc) 2010, tit.petric@monotek.net
 http://creativecommons.org/licenses/by-sa/3.0/
 
+Updated by: William Hetherington, NetroMedia, will@netromedia.com
+
 */
 
-package com.monotek.wms.module;
+package com.monotek.wms.module; 
 
 import com.maxmind.geoip.*;
 
@@ -34,14 +36,48 @@ class GeoIPLookupService
 	}
 
 	/** Validate Country code against IPAdress's country code */
-	public boolean ValidateCountry(String IPAddress, String CountryCode)
+	public boolean ValidateCountry(String IPAddress, String CountryName)
 	{
-		String IPCountryCode = "--";
+		String IPCountryName = "--";
 		try {
-			IPCountryCode = myLookupService.getCountry(IPAddress).getCode();
+			//IPCountryCode = myLookupService.getCountry(IPAddress).getCode();
+			Location myLocation = myLookupService.getLocation(IPAddress);
+			IPCountryName = myLocation.countryCode;
 		} catch (Exception e) {
 			return false;
 		}
-		return IPCountryCode.equals(CountryCode);
+		return IPCountryName.equals(CountryName);
+	}
+	
+	/** Validate Country & Region code */
+	public boolean ValidateRegion(String IPAddress, String CountryName, String RegionName)
+	{
+		String IPCountryName = "";
+		String IPRegionName = "";
+		try {
+			Location myLocation = myLookupService.getLocation(IPAddress);
+			IPCountryName = myLocation.countryCode;
+			IPRegionName = myLocation.region;
+		} catch (Exception e) {
+			return false;
+		}
+		return IPCountryName.equals(CountryName) && IPRegionName.equals(RegionName);
+	}
+	
+	/** Validate a city */
+	public boolean ValidateCity(String IPAddress, String CountryName, String RegionName, String CityName)
+	{
+		String IPCountryName = "";
+		String IPRegionName = "";
+		String IPCityName = "";
+		try {
+			Location myLocation = myLookupService.getLocation(IPAddress);
+			IPCountryName = myLocation.countryCode;
+			IPRegionName = myLocation.region;
+			IPCityName = myLocation.city;
+		} catch (Exception e) {
+			return false;
+		}
+		return IPCountryName.equals(CountryName) && IPRegionName.equals(RegionName) && IPCityName.equals(CityName);
 	}
 }
